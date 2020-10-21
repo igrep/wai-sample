3e466deb7f6a3384254d9238b0f55a75f687914f
YAMAMOTO Yuji <yuji-yamamoto@iij.ad.jp>
2020-10-21 18:44:24 +0900

    Add simplest routing table

--- src/WaiSample.hs
+++ src/WaiSample.hs
@@ -4,13 +4,32 @@ module WaiSample
   ( app
   ) where
 
-import           Control.Exception (bracket_)
-import           Network.HTTP.Types.Status (status200)
-import           Network.Wai (Application, responseLBS)
+import           Control.Exception         (bracket_)
+import           Data.List                 (find)
+import qualified Data.Text                 as T
+import           Network.HTTP.Types.Status (status200, status404)
+import           Network.Wai               (Application, Request, Response,
+                                            pathInfo, responseLBS)
 
 
 app :: Application
-app _req respond = bracket_
-  (putStrLn "Allocating")
-  (putStrLn "Cleaning")
-  (respond $ responseLBS status200 [] "Hello, World!")
+app = handles
+  [ ([], \_req -> return $ responseLBS status200 [] "index")
+  , (["about", "us"], \_req -> return $ responseLBS status200 [] "About IIJ")
+  ]
+
+
+type Handler = ([T.Text], Request -> IO Response)
+
+-- TODO: Create a type convertible to `Request -> Maybe a` and the expected paths are machine readable
+-- type Handler2 = (Request -> Maybe a, a -> IO Response)
+
+-- type Handler3 = Request -> Maybe (IO Response)
+
+
+handles :: [Handler] -> Application
+handles handlers req respond = bracket_ (putStrLn "Allocating") (putStrLn "Cleaning") $ do
+  let mhandler = snd <$> find (\(path, _action) -> path == pathInfo req) handlers
+  case mhandler of
+      Just handler -> respond =<< handler req
+      Nothing      -> respond $ responseLBS status404 [] "404 Not found."
