7c4337ba83db2dc3c909fbd25db620752ff139b8
YAMAMOTO Yuji <yuji-yamamoto@iij.ad.jp>
2020-10-28 18:20:11 +0900

    New router using ExistentialQuantification

--- src/WaiSample.hs
+++ src/WaiSample.hs
@@ -1,11 +1,15 @@
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ExistentialQuantification #-}
+{-# LANGUAGE OverloadedStrings         #-}
 
 module WaiSample
   ( app
+  , Handler (..)
   ) where
 
 import           Control.Exception         (bracket_)
+import           Control.Monad             (guard)
 import           Data.List                 (find)
+import           Data.Maybe                (fromJust, isJust)
 import qualified Data.Text                 as T
 import           Network.HTTP.Types.Status (status200, status404)
 import           Network.Wai               (Application, Request, Response,
@@ -14,22 +18,33 @@ import           Network.Wai               (Application, Request, Response,
 
 app :: Application
 app = handles
-  [ ([], \_req -> return $ responseLBS status200 [] "index")
-  , (["about", "us"], \_req -> return $ responseLBS status200 [] "About IIJ")
+  [ (pathIs []) `then'` (\_ -> return $ responseLBS status200 [] "index")
+  , (pathIs ["about", "us"]) `then'` (\_ -> return $ responseLBS status200 [] "About IIJ")
   ]
 
 
-type Handler = ([T.Text], Request -> IO Response)
+pathIs :: [T.Text] -> Request -> Maybe ()
+pathIs ps req = guard $ pathInfo req == ps
+
 
 -- TODO: Create a type convertible to `Request -> Maybe a` and the expected paths are machine readable
--- type Handler2 = (Request -> Maybe a, a -> IO Response)
+data Handler = forall a. Handler
+  { match   :: Request -> Maybe a
+  , respond :: a -> IO Response
+  }
 
--- type Handler3 = Request -> Maybe (IO Response)
+newtype Handler3 = Handler3 { runHandler3 :: Request -> Maybe (IO Response) }
+
+instance Semigroup Handler3 where
+  a <> b = undefined
+
+then' :: (Request -> Maybe a) -> (a -> IO Response) -> Handler
+then' = Handler
 
 
 handles :: [Handler] -> Application
-handles handlers req respond = bracket_ (putStrLn "Allocating") (putStrLn "Cleaning") $ do
-  let mhandler = snd <$> find (\(path, _action) -> path == pathInfo req) handlers
+handles handlers req respond' = bracket_ (putStrLn "Allocating") (putStrLn "Cleaning") $ do
+  let mhandler = find (\(Handler mat _res) -> isJust $ mat req) handlers
   case mhandler of
-      Just handler -> respond =<< handler req
-      Nothing      -> respond $ responseLBS status404 [] "404 Not found."
+      Just (Handler mat res) -> respond' =<< (res . fromJust $ mat req)
+      Nothing                -> respond' $ responseLBS status404 [] "404 Not found."
