a32f4150b4a9386cddef8d5580ca2f39dfe090da
YAMAMOTO Yuji <yuji-yamamoto@iij.ad.jp>
2020-11-04 18:42:06 +0900

    Integrate input handlers and output handlers with a type variable

--- src/WaiSample.hs
+++ src/WaiSample.hs
@@ -8,8 +8,8 @@ module WaiSample
 
 import           Control.Exception         (bracket_)
 import           Control.Monad             (guard)
-import           Data.List                 (find)
-import           Data.Maybe                (fromJust, isJust)
+import           Data.Maybe                (listToMaybe, mapMaybe)
+import           Data.Monoid               (First (First, getFirst))
 import qualified Data.Text                 as T
 import           Network.HTTP.Types.Status (status200, status404)
 import           Network.Wai               (Application, Request, Response,
@@ -23,28 +23,35 @@ app = handles
   ]
 
 
-pathIs :: [T.Text] -> Request -> Maybe ()
-pathIs ps req = guard $ pathInfo req == ps
+pathIs :: [T.Text] -> Handler ()
+pathIs ps = Handler $ \req -> guard $ pathInfo req == ps
+
+{-
+  pathIs :: PathHandler a -> Handler a
+  pathPiece :: T.Text -> PathHandler ()
+-}
 
 
--- TODO: Create a type convertible to `Request -> Maybe a` and the expected paths are machine readable
-data Handler = forall a. Handler
-  { match   :: Request -> Maybe a
-  , respond :: a -> IO Response
-  }
-
-newtype Handler3 = Handler3 { runHandler3 :: Request -> Maybe (IO Response) }
-
-instance Semigroup Handler3 where
-  a <> b = undefined
-
-then' :: (Request -> Maybe a) -> (a -> IO Response) -> Handler
-then' = Handler
+-- /for/example/users/:id
+intPath :: Handler Int
+intPath = undefined
 
 
-handles :: [Handler] -> Application
+newtype Handler a = Handler { runHandler :: Request -> Maybe a }
+
+instance Semigroup (Handler a) where
+  (Handler a) <> (Handler b) = Handler $ \req -> getFirst $ First (a req) <> First (b req)
+
+
+then' :: Handler a -> (a -> IO Response) -> Handler (IO Response)
+then' = undefined
+
+
+handles :: [Handler (IO Response)] -> Application
 handles handlers req respond' = bracket_ (putStrLn "Allocating") (putStrLn "Cleaning") $ do
-  let mhandler = find (\(Handler mat _res) -> isJust $ mat req) handlers
-  case mhandler of
-      Just (Handler mat res) -> respond' =<< (res . fromJust $ mat req)
-      Nothing                -> respond' $ responseLBS status404 [] "404 Not found."
+  let foundResponds = listToMaybe $ mapMaybe (\handler -> runHandler handler req) handlers
+  case foundResponds of
+      Just respond -> respond' =<< respond
+      Nothing      -> respond' handle404
+ where
+  handle404 = responseLBS status404 [] "404 Not found."
