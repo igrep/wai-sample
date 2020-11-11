9ecc789da1db43daf142ab6d418cbe7e4cd67b62
YAMAMOTO Yuji <yuji-yamamoto@iij.ad.jp>
2020-11-11 18:45:22 +0900

    (WIP) Add PathParser, dedicated Handler for pathInfo of WAI

--- src/WaiSample.hs
+++ src/WaiSample.hs
@@ -3,6 +3,10 @@
 
 module WaiSample
   ( app
+  , anyPiece
+  , path
+  , piece
+  , decimalPiece
   , Handler (..)
   ) where
 
@@ -11,6 +15,7 @@ import           Control.Monad             (guard)
 import           Data.Maybe                (listToMaybe, mapMaybe)
 import           Data.Monoid               (First (First, getFirst))
 import qualified Data.Text                 as T
+import qualified Data.Text.Read            as TR
 import           Network.HTTP.Types.Status (status200, status404)
 import           Network.Wai               (Application, Request, Response,
                                             pathInfo, responseLBS)
@@ -26,16 +31,46 @@ app = handles
 pathIs :: [T.Text] -> Handler ()
 pathIs ps = Handler $ \req -> guard $ pathInfo req == ps
 
-{-
-  pathIs :: PathHandler a -> Handler a
-  pathPiece :: T.Text -> PathHandler ()
--}
+
+newtype PathParser a = PathParser { runPathParser :: [T.Text] -> Maybe (a, [T.Text]) }
 
 
--- /for/example/users/:id
-intPath :: Handler Int
-intPath = undefined
+anyPiece :: PathParser T.Text
+anyPiece = PathParser $ \inp ->
+  case inp of
+      []       -> Nothing
+      (p : ps) -> Just (p, ps)
 
+
+pieceSatisfying :: (T.Text -> Bool) -> PathParser T.Text
+pieceSatisfying predicate = PathParser $ \inp -> do
+  (p, ps) <- runPathParser anyPiece inp
+  if predicate p then Just (p, ps) else Nothing
+
+
+piece :: T.Text -> PathParser T.Text
+piece p = pieceSatisfying (== p)
+
+
+-- piece "foo" *> piece "example" *> piece "users"
+-- pathIs "foo/example/users" *> decimalPiece
+
+-- :id of /for/example/users/:id
+decimalPiece :: PathParser Int
+decimalPiece = PathParser $ \inp -> do
+  (p, ps) <- runPathParser anyPiece inp
+  case TR.decimal p of
+      Right (i, "") -> Just (i, ps)
+      Right _       -> Nothing
+      Left _        -> Nothing
+
+
+path :: T.Text -> PathParser [T.Text]
+path pathWithSlash = undefined -- PathParser $ \inp -> do
+  -- TODO: Implment using instance of Applicative and Functor
+ where
+  -- TODO: Explain dropWhile, dropWhileEnd, dropAround
+  ps = T.split (== '/') $ T.dropAround (== '/') pathWithSlash
 
 newtype Handler a = Handler { runHandler :: Request -> Maybe a }
 
