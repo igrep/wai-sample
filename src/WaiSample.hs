0b75b746f16327b4c5d846a1d3faca082a42f34a
YAMAMOTO Yuji <yuji-yamamoto@iij.ad.jp>
2020-11-25 18:35:39 +0900

    Implement PathParser, with a bug left

--- src/WaiSample.hs
+++ src/WaiSample.hs
@@ -8,31 +8,43 @@ module WaiSample
   , piece
   , decimalPiece
   , Handler (..)
+  , PathParser (..)
   ) where
 
-import           Control.Exception         (bracket_)
-import           Control.Monad             (guard)
-import           Data.Maybe                (listToMaybe, mapMaybe)
-import           Data.Monoid               (First (First, getFirst))
-import qualified Data.Text                 as T
-import qualified Data.Text.Read            as TR
-import           Network.HTTP.Types.Status (status200, status404)
-import           Network.Wai               (Application, Request, Response,
-                                            pathInfo, responseLBS)
+import           Control.Exception          (bracket_)
+import           Control.Monad              (guard)
+import           Data.Bifunctor             (first)
+import qualified Data.ByteString.Lazy.Char8 as BL
+import           Data.Maybe                 (listToMaybe, mapMaybe)
+import           Data.Monoid                (First (First, getFirst))
+import qualified Data.Text                  as T
+import qualified Data.Text.Read             as TR
+import           Network.HTTP.Types.Status  (status200, status404)
+import           Network.Wai                (Application, Request, Response,
+                                             pathInfo, responseLBS)
 
 
 app :: Application
 app = handles
-  [ (pathIs []) `then'` (\_ -> return $ responseLBS status200 [] "index")
-  , (pathIs ["about", "us"]) `then'` (\_ -> return $ responseLBS status200 [] "About IIJ")
+  [ path "/" `then'` (\_ -> return $ responseLBS status200 [] "index")
+  , path "/about/us" `then'` (\_ -> return $ responseLBS status200 [] "About IIJ")
+  , path "/about/us/finance" `then'` (\_ -> return $ responseLBS status200 [] "Financial Report 2020")
+  , (path "/customer/" *> decimalPiece) `then'`
+      (\i -> return $ responseLBS status200 [] $ "Customer ID: " <> BL.pack (show i))
   ]
 
 
-pathIs :: [T.Text] -> Handler ()
-pathIs ps = Handler $ \req -> guard $ pathInfo req == ps
-
-
 newtype PathParser a = PathParser { runPathParser :: [T.Text] -> Maybe (a, [T.Text]) }
+
+instance Functor PathParser where
+  fmap f p = PathParser $ \inp ->
+    first f <$> runPathParser p inp
+
+instance Applicative PathParser where
+  pure x = PathParser $ \inp -> Just (x, inp)
+  pf <*> px = PathParser $ \inp -> do
+    (f, out) <- runPathParser pf inp
+    runPathParser (fmap f px) out
 
 
 anyPiece :: PathParser T.Text
@@ -53,7 +65,7 @@ piece p = pieceSatisfying (== p)
 
 
 -- piece "foo" *> piece "example" *> piece "users"
--- pathIs "foo/example/users" *> decimalPiece
+-- path "foo/example/users" *> decimalPiece
 
 -- :id of /for/example/users/:id
 decimalPiece :: PathParser Int
@@ -65,12 +77,18 @@ decimalPiece = PathParser $ \inp -> do
       Left _        -> Nothing
 
 
+-- TODO: Handle PathInfo including empty path pieces correctly.
 path :: T.Text -> PathParser [T.Text]
-path pathWithSlash = undefined -- PathParser $ \inp -> do
-  -- TODO: Implment using instance of Applicative and Functor
+path pathWithSlash = traverse piece ps
  where
-  -- TODO: Explain dropWhile, dropWhileEnd, dropAround
   ps = T.split (== '/') $ T.dropAround (== '/') pathWithSlash
+
+
+pathParserToHandler :: PathParser a -> Handler a
+pathParserToHandler p = Handler $ \req -> do
+  (x, left) <- runPathParser p $ pathInfo req
+  guard $ left == []
+  return x
 
 newtype Handler a = Handler { runHandler :: Request -> Maybe a }
 
@@ -78,8 +96,8 @@ instance Semigroup (Handler a) where
   (Handler a) <> (Handler b) = Handler $ \req -> getFirst $ First (a req) <> First (b req)
 
 
-then' :: Handler a -> (a -> IO Response) -> Handler (IO Response)
-then' = undefined
+then' :: PathParser a -> (a -> IO Response) -> Handler (IO Response)
+then' p act = Handler $ fmap act . runHandler (pathParserToHandler p)
 
 
 handles :: [Handler (IO Response)] -> Application
