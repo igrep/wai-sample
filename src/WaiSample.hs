4426cdd964bb729c0530d13d3a5aee531101a020
YAMAMOTO Yuji <yuji-yamamoto@iij.ad.jp>
2021-01-27 18:26:18 +0900

    Add ToFromRequestBody, to automate conversion between Handlers' result and response body

--- src/WaiSample.hs
+++ src/WaiSample.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE DeriveGeneric             #-}
 {-# LANGUAGE ExistentialQuantification #-}
 {-# LANGUAGE GADTs                     #-}
 {-# LANGUAGE OverloadedStrings         #-}
@@ -16,15 +17,19 @@ module WaiSample
 
 import           Control.Exception          (bracket_)
 import           Control.Monad              (guard)
+import           Data.Aeson                 (FromJSON, ToJSON)
+import qualified Data.Aeson                 as Json
 import           Data.Bifunctor             (first)
 import qualified Data.ByteString.Lazy.Char8 as BL
 import           Data.Functor               (void)
 import           Data.Maybe                 (listToMaybe, mapMaybe)
 import           Data.Monoid                (First (First, getFirst))
 import qualified Data.Text                  as T
+import qualified Data.Text.Encoding         as TE
 import qualified Data.Text.Lazy.Builder     as TLB
 import qualified Data.Text.Lazy.IO          as TLIO
 import qualified Data.Text.Read             as TR
+import           GHC.Generics               (Generic)
 import           Network.HTTP.Types.Status  (status200, status404)
 import           Network.Wai                (Application, Request, Response,
                                              pathInfo, responseLBS)
@@ -36,16 +41,33 @@ app :: Application
 app = handles routes
 
 
-routes :: [Handler (IO Response)]
+routes :: [Handler]
 routes =
-  [ path "/" `then'` (\_ -> return $ responseLBS status200 [] "index")
-  , path "/about/us" `then'` (\_ -> return $ responseLBS status200 [] "About IIJ")
-  , path "/about/us/finance" `then'` (\_ -> return $ responseLBS status200 [] "Financial Report 2020")
-  , path "/about/finance" `then'` (\_ -> return $ responseLBS status200 [] "Financial Report 2020 /")
-  , path "/about//finance" `then'` (\_ -> fail "This should not be executed.")
-  , (path "/customer/" *> decimalPiece) `then'`
-      (\i -> return $ responseLBS status200 [] $ "Customer ID: " <> BL.pack (show i))
+  [ (path "/" `then'` (\_ -> return "index")) text
+  , (path "/about/us" `then'` (\_ -> return "About IIJ")) text
+  , (path "/about/us/finance" `then'` (\_ -> return "Financial Report 2020")) text
+  , (path "/about/finance" `then'` (\_ -> return "Financial Report 2020 /")) text
+  , (path "/about//finance" `then'` (\_ -> fail "This should not be executed.")) text
+  , ((path "/customer/" *> decimalPiece) `then'`
+      (\i -> return $ "Customer ID: " <> T.pack (show i))) text
+  , ((path "/customer/" *> decimalPiece <* Piece "json" ) `then'`
+      (\i -> return $ Customer
+        { customerName = "Mr. " <> T.pack (show i)
+        , customerId = i
+        })) json
   ]
+
+
+data Customer = Customer
+  { customerName :: T.Text
+  , customerId   :: Integer
+  } deriving (Generic, Show)
+-- TODO: Genericについて解説
+
+instance ToJSON Customer where
+  toEncoding = Json.genericToEncoding Json.defaultOptions
+
+instance FromJSON Customer
 
 
 printRoutes :: IO ()
@@ -105,18 +127,40 @@ pathWithSlashes pathWithSlash = traverse piece ps
 
 runRoutingTable :: RoutingTable a -> Request -> Maybe a
 runRoutingTable p req = do
+  -- TODO: Redirect when finding consecutive slashes.
+  -- TODO: Join pathInfo, then parse as raw path string.
   (x, left) <- parseByRoutingTable p . filter (/= "") $ pathInfo req
   guard $ left == []
   return x
 
-data Handler a where
-  ThenHandler :: RoutingTable a -> (a -> IO Response) -> Handler (IO Response)
+data Handler where
+  ThenHandler :: RoutingTable a -> (a -> IO resObj) -> ToFromResponseBody resObj -> Handler
 
-then' :: RoutingTable a -> (a -> IO Response) -> Handler (IO Response)
+data ToFromResponseBody resObj = ToFromResponseBody
+  { toResponseBody   :: resObj -> IO BL.ByteString
+  , fromResponseBody :: BL.ByteString -> IO resObj
+  }
+  -- TODO: Add mimetype
+  -- TODO: Add Negotiation with Content-Type
+  -- TODO: Add other header, status code etc.
+
+json :: (ToJSON resObj, FromJSON resObj) => ToFromResponseBody resObj
+json = ToFromResponseBody
+  { toResponseBody = return . Json.encode
+  , fromResponseBody = either fail return . Json.eitherDecode'
+  }
+
+text :: ToFromResponseBody T.Text
+text = ToFromResponseBody
+  { toResponseBody = return . BL.fromStrict . TE.encodeUtf8
+  , fromResponseBody = return . TE.decodeUtf8 . BL.toStrict
+  }
+
+then' :: RoutingTable a -> (a -> IO resObj) -> ToFromResponseBody resObj -> Handler
 then' = ThenHandler
 
 
-handles :: [Handler (IO Response)] -> Application
+handles :: [Handler] -> Application
 handles hdls req respond' = bracket_ (putStrLn "Allocating") (putStrLn "Cleaning") $ do
   let foundResponds = listToMaybe $ mapMaybe (\hdl -> runHandler hdl req) hdls
   case foundResponds of
@@ -126,8 +170,13 @@ handles hdls req respond' = bracket_ (putStrLn "Allocating") (putStrLn "Cleaning
   handle404 = responseLBS status404 [] "404 Not found."
 
 
-runHandler :: Handler (IO Response) -> Request -> Maybe (IO Response)
-runHandler (ThenHandler tbl hdl) req = hdl <$> runRoutingTable tbl req
+runHandler :: Handler -> Request -> Maybe (IO Response)
+runHandler (ThenHandler tbl hdl tfr) req =
+  act <$> runRoutingTable tbl req
+ where
+  act x = do
+    resBody <- toResponseBody tfr =<< hdl x
+    return $ responseLBS status200 [] resBody
 
 
 parseByRoutingTable :: RoutingTable a -> [T.Text] -> Maybe (a, [T.Text])
@@ -151,7 +200,7 @@ parseByRoutingTable (ParsedPath parser) = \inp ->
 
 
 -- TODO: Delete extra slashes and newlines
-showRoutes :: [Handler a] -> TLB.Builder
+showRoutes :: [Handler] -> TLB.Builder
 showRoutes = ("/" <>) . foldMap ((<> "\n/") . showRoutes' . extractRoutingTable)
 
 showRoutes' :: RoutingTable a -> TLB.Builder
@@ -163,5 +212,5 @@ showRoutes' (ApPath tblF tblA)   = showRoutes' tblF <> "/" <> showRoutes' tblA
 showRoutes' (AltPath tblA tblB)  = showRoutes' tblA <> "\n/" <> showRoutes' tblB
 showRoutes' (ParsedPath _parser) = ":param" -- TODO: Name the parameter
 
-extractRoutingTable :: Handler a -> RoutingTable ()
-extractRoutingTable (ThenHandler tbl _) = void tbl
+extractRoutingTable :: Handler -> RoutingTable ()
+extractRoutingTable (ThenHandler tbl _hdl _tfr) = void tbl
