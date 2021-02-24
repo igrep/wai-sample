e473a90bc350d7840666851fdcc2086a2a9c7866
YAMAMOTO Yuji <yuji-yamamoto@iij.ad.jp>
2021-02-24 18:25:34 +0900

    Compile declareClient forcibly
    The generated functions don't type check yet, but I want to show any working example of TemplateHaskell

--- src/WaiSample/Client.hs
+++ src/WaiSample/Client.hs
@@ -4,27 +4,25 @@ module WaiSample.Client
   , Backend
   ) where
 
-import           Control.Monad        (foldM)
 import qualified Data.ByteString.Lazy as BL
 import           Data.Char            (toUpper)
-import           Language.Haskell.TH  (Dec, DecQ, DecsQ, mkName, sigD)
+import qualified Data.Text            as T
+import           Language.Haskell.TH  (Dec, DecQ, DecsQ, clause, funD, mkName,
+                                       normalB, sigD)
 import           WaiSample
 
 
 declareClient :: String -> [Handler] -> DecsQ
-declareClient prefix = foldM f []
+declareClient prefix = fmap concat . mapM declareEndpointFunction
  where
-  f :: [Dec] -> Handler -> DecsQ
-  f decs handler = do
-    sigDAndFunD <- declareEndpointFunction handler
-    return $ sigDAndFunD ++ decs
-
   declareEndpointFunction :: Handler -> DecsQ
   declareEndpointFunction (Handler handlerName tbl _action toFromResponseBody) = do
-    funName <- mkName $ makeUpName handlerName
-    [d|
-      $(sigD funName [t| Backend |])
-      |]
+    let funName = mkName $ makeUpName handlerName
+    sig <- sigD funName [t| Backend |]
+    def <- funD
+      funName
+      [clause [[p| bd |]] (normalB [e| return $ T.pack "" |]) []]
+    return [sig, def]
 
   makeUpName :: String -> String
   makeUpName handlerName =
