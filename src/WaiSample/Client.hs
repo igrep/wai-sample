5f232e053a23742e9e3d634f3ce584a2490fef26
YAMAMOTO Yuji <yuji-yamamoto@iij.ad.jp>
2021-03-03 18:38:45 +0900

    (WIP) Build a sigD from typeReps in a Handler object

--- src/WaiSample/Client.hs
+++ src/WaiSample/Client.hs
@@ -7,8 +7,9 @@ module WaiSample.Client
 import qualified Data.ByteString.Lazy as BL
 import           Data.Char            (toUpper)
 import qualified Data.Text            as T
-import           Language.Haskell.TH  (Dec, DecQ, DecsQ, clause, funD, mkName,
-                                       normalB, sigD)
+import           Data.Typeable        (TypeRep)
+import           Language.Haskell.TH  (Dec, DecQ, DecsQ, TypeQ, appT, clause,
+                                       funD, mkName, normalB, sigD)
 import           WaiSample
 
 
@@ -18,7 +19,9 @@ declareClient prefix = fmap concat . mapM declareEndpointFunction
   declareEndpointFunction :: Handler -> DecsQ
   declareEndpointFunction (Handler handlerName tbl _action toFromResponseBody) = do
     let funName = mkName $ makeUpName handlerName
-    sig <- sigD funName [t| Backend |]
+        typeQUrl = typeRepToTypeQ $ getTypeRep tbl
+    -- TODO: 戻り値のTypeQ, 引数のRepが()の場合は引数に追加しない
+    sig <- sigD funName $ [t| (->) |] `appT` [t| Backend |] `appT` typeQUrl
     def <- funD
       funName
       [clause [[p| bd |]] (normalB [e| return $ T.pack "" |]) []]
@@ -33,6 +36,11 @@ declareClient prefix = fmap concat . mapM declareEndpointFunction
   toUpperFirst :: String -> String
   toUpperFirst (first : left) = toUpper first : left
   toUpperFirst _              = error "toUpperFirst: Empty handler name!"
+
+
+typeRepToTypeQ :: TypeRep -> TypeQ
+typeRepToTypeQ _ =
+  error "typeRepToTypeQ is not defined yet!"
 
 
 type Backend = String -> IO BL.ByteString
