a5e4c28876b6274fd1c5c0bff7ecd9bf130b419c
YAMAMOTO Yuji <yuji-yamamoto@iij.ad.jp>
2021-04-14 18:40:42 +0900

    Generate argument names of the path builder, and some of its implementation

--- src/WaiSample/Client.hs
+++ src/WaiSample/Client.hs
@@ -6,15 +6,15 @@ module WaiSample.Client
   ) where
 
 import qualified Data.ByteString.Lazy       as BL
-import           Data.Char                  (toUpper)
+import           Data.Char                  (toLower, toUpper)
 import           Data.Proxy                 (Proxy (Proxy))
 import qualified Data.Text                  as T
 import           Data.Typeable              (TypeRep, tyConModule, tyConName,
                                              tyConPackage, typeOf, typeRep,
                                              typeRepTyCon)
-import           Language.Haskell.TH        (DecsQ, ExpQ, TypeQ, appT, clause,
-                                             conT, funD, mkName, normalB, sigD,
-                                             varE, varP)
+import           Language.Haskell.TH        (DecsQ, ExpQ, Q, TypeQ, appE, appT,
+                                             clause, conT, funD, mkName,
+                                             newName, normalB, sigD, varE, varP)
 import           Language.Haskell.TH.Syntax (ModName (ModName), Name (Name),
                                              NameFlavour (NameG),
                                              NameSpace (TcClsName),
@@ -40,16 +40,17 @@ declareClient prefix = fmap concat . mapM declareEndpointFunction
     sig <- sigD funName $  [t| Backend |] `funcT` typeQFromRoutingTable typeQTail tbl
 
     let bd = mkName "bd"
-        p = pathBuilderFromRoutingTable tbl
-        -- TODO: generate arguments `VarP`s
-        implE = [e| \aaa ->
+        moreArgs = argumentNamesFromRoutingTable tbl
+        allArgs = varP bd : map (varP =<<) moreArgs
+        p = pathBuilderFromRoutingTable moreArgs tbl
+        implE = [e|
             do
-              resBody <- $(varE bd) $ $(p) aaa
+              resBody <- $(varE bd) $(p)
               return $ fromResponseBody resBody
           |]
     def <- funD
       funName
-      [clause [varP bd] (normalB implE) []]
+      [clause allArgs (normalB implE) []]
     return [sig, def]
 
   makeUpName :: String -> String
@@ -78,16 +79,36 @@ typeQFromRoutingTable typeQTail = foldr funcT typeQTail  . reverse . go []
   go tqs (ParsedPath proxy)   = typeRepToTypeQ (typeRep proxy) : tqs
 
 
-pathBuilderFromRoutingTable :: RoutingTable a -> ExpQ
-pathBuilderFromRoutingTable = foldr build [e| "" |] . reverse . go []
+argumentNamesFromRoutingTable :: RoutingTable a -> [Q Name]
+argumentNamesFromRoutingTable = reverse . go []
+ where
+  go :: [Q Name] -> RoutingTable b -> [Q Name]
+  go tqs AnyPiece             = newName "any" : tqs
+  go tqs (Piece _p)           = tqs
+  go tqs (FmapPath _f tbl)    = go tqs tbl
+  go tqs (PurePath _x)        = tqs
+  go tqs (ApPath tblF tblA)   =
+    let tqs' = go tqs tblF
+     in go tqs' tblA
+  go tqs (ParsedPath proxy)   = typeRepToNameQ (typeRep proxy) : tqs
+
+
+-- TODO: 今回は文字列を組み立てるので、foldrを使う必要はなさそう
+pathBuilderFromRoutingTable :: [Q Name] -> RoutingTable a -> ExpQ
+pathBuilderFromRoutingTable argNames = foldr build [e| "" |] . reverse . go argNames []
  where
   -- TODO
   build :: ExpQ -> ExpQ -> ExpQ
   build = undefined
 
   -- TODO
-  go :: [TypeQ] -> RoutingTable b -> [ExpQ]
-  go = undefined
+  go :: [Q Name] -> [ExpQ] -> RoutingTable b -> [ExpQ]
+  go (arg0 : args) eqs AnyPiece             = undefined
+  go  args         eqs (Piece p)            = undefined
+  go  args         eqs (FmapPath _f tbl)    = go args eqs tbl
+  go  args         eqs (PurePath _x)        = undefined
+  go  args         eqs (ApPath tblF tblA)   = undefined
+  go (arg0 : args) eqs (ParsedPath _parser) = undefined
 
 {-
 pathBuilderFromRoutingTable :: RoutingTable a -> String
@@ -111,6 +132,17 @@ typeRepToTypeQ rep = conT $ Name occName nameFlavour
       (PkgName $ tyConPackage tyCon)
       (ModName $ tyConModule tyCon)
   tyCon = typeRepTyCon rep
+
+
+typeRepToNameQ :: TypeRep -> Q Name
+typeRepToNameQ rep = newName namePrefix
+ where
+  namePrefix = toLowerFirst $ tyConName tyCon
+  tyCon = typeRepTyCon rep
+
+  toLowerFirst :: String -> String
+  toLowerFirst (first : left) = toLower first : left
+  toLowerFirst _              = error "toLowerFirst: Empty handler name!"
 
 
 -- | Generate a type 'a -> b'
