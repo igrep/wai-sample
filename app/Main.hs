6039ddc70ca438f4c01c72717667aba02cbfd632
YAMAMOTO Yuji <igrep@n.email.ne.jp>
2022-03-16 18:44:02 +0900

    Fix more compiler error

--- app/Main.hs
+++ app/Main.hs
@@ -2,7 +2,7 @@ module Main where
 
 import           Network.Wai.Handler.Warp (runEnv)
 
-import           WaiSample (sampleApp)
+import           WaiSample.Server         (sampleApp)
 
 main :: IO ()
 main = runEnv 8080 sampleApp
