a6f9163a492830f33ce6638fb707447bf94e6b70
YAMAMOTO Yuji <igrep@n.email.ne.jp>
2022-03-30 18:17:00 +0900

    (STILL HAS TYPE ERRORS) Add more notes to explain DataKinds, TypeFamilies etc, Fix type errors in `Elem` and `CheckElemIsMember`

--- src/WaiSample/Types/Response/Sum.hs
+++ src/WaiSample/Types/Response/Sum.hs
@@ -1,14 +1,15 @@
-{-# LANGUAGE DataKinds      #-}
-{-# LANGUAGE GADTs          #-}
-{-# LANGUAGE KindSignatures #-}
-{-# LANGUAGE TypeOperators  #-}
+{-# LANGUAGE DataKinds            #-}
+{-# LANGUAGE GADTs                #-}
+{-# LANGUAGE TypeFamilies         #-}
+{-# LANGUAGE TypeOperators        #-}
+{-# LANGUAGE UndecidableInstances #-}
 
 -- | Ref. https://github.com/cdepillabout/world-peace/blob/0596da67d792ccf9f0ddbe44b5ce71b38cbde020/src/Data/WorldPeace/Union.hs
 module WaiSample.Types.Response.Sum where
 
-import           Data.Kind (Constraint)
+import           Data.Kind      (Constraint)
 import           Data.Type.Bool (If)
-import           GHC.TypeLits (ErrorMessage(..), TypeError)
+import           GHC.TypeLits   (ErrorMessage (..), TypeError)
 
 
 -- [1, 2, 3] :: [Int]
@@ -16,7 +17,24 @@ import           GHC.TypeLits (ErrorMessage(..), TypeError)
 -- [Bool, Int, String] :: [*]
 
 -- data [a] = [] | a : [a]
+--      ↓ 昇格
 -- kind [a] = '[] | a ': [a]
+
+-- data Bool = True | False
+--      ↓ 昇格
+-- kind Bool = 'True | 'False
+
+{-
+  Left (Right 1) :: Either (Either Bool Int) String
+  That (This 1) :: Union '[String, Int]
+  That (This 1) :: Union '[String, Int, Maybe Bool,]
+  That (This 1) :: Union '[]
+                          String ': [Int]
+                          String ': Int ': '[]
+                          (String, Int)
+                          ["abc", "def"] :: [String]
+                          "abc" : ["def"]
+-}
 
 data Sum (as :: [*]) where
   This :: a -> Sum (a ': as)
@@ -26,11 +44,13 @@ data Sum (as :: [*]) where
 -- | This is a helpful 'Constraint' synonym to assert that @a@ is a member of
 -- @as@.  You can see how it is used in functions like 'openUnionLift'.
 type IsMember (a :: *) (as :: [*]) = (CheckElemIsMember a as, UElem a as (RIndex a as))
+-- ConstraintKinds
 
 
 -- | This type family checks whether @a@ is inside @as@ and produces
 -- compile-time error if not.
-type family CheckElemIsMember :: * -> [*] -> Constraint where
+-- type family CheckElemIsMember :: * -> [*] -> Constraint where
+type family CheckElemIsMember (a :: *) (as :: [*]) :: Constraint where
   CheckElemIsMember a as =
     If (Elem a as) (() :: Constraint) (TypeError (NoElementError a as))
 
@@ -41,7 +61,8 @@ type family CheckElemIsMember :: * -> [*] -> Constraint where
 -- Refl
 -- >>> Refl :: Elem String '[Double, Char] :~: 'False
 -- Refl
-type family Elem :: * -> [*] -> Bool where
+-- type family Elem :: * -> [*] -> Bool where
+type family Elem (a :: *) (as :: [*]) :: Bool where
   Elem _ '[]       = 'False
   Elem x (x ': xs) = 'True
   Elem x (y ': xs) = Elem x xs
