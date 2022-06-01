84ecc1827db8fac0fd45d4fbe0286908ef600b3a
YAMAMOTO Yuji <igrep@n.email.ne.jp>
2022-06-01 18:29:12 +0900

    (WIP) (HAS TYPE ERROR) Redesign Content-Type-related types

--- src/WaiSample.hs
+++ src/WaiSample.hs
@@ -61,7 +61,7 @@ sampleRoutes =
   , get "aboutFinanceImpossible" (path "/about/finance/impossible") (Proxy :: Proxy PlainText) (\_ -> (fail "This should not be executed due to the leading slash" :: IO T.Text))
   , get "customerId"
       (path "customer/" *> decimalPiece)
-      (Proxy :: (Proxy (Json |&| FormUrlEncoded)))
+      (Proxy :: (Proxy (ContentTypes '[Json, FormUrlEncoded])))
       (return . customerOfId)
   , get "customerIdJson"
     -- /customer/:id.json
--- src/WaiSample/Types/ContentTypes.hs
+++ src/WaiSample/Types/ContentTypes.hs
@@ -1,22 +1,33 @@
-{-# LANGUAGE DeriveLift          #-}
-{-# LANGUAGE OverloadedStrings   #-}
-{-# LANGUAGE ScopedTypeVariables #-}
-{-# LANGUAGE TypeOperators       #-}
+{-# LANGUAGE DeriveLift                #-}
+{-# LANGUAGE ExistentialQuantification #-}
+{-# LANGUAGE OverloadedStrings         #-}
+{-# LANGUAGE ScopedTypeVariables       #-}
+{-# LANGUAGE TypeOperators             #-}
 
 module WaiSample.Types.ContentTypes where
 
+import qualified Data.Foldable              as F
 import           Data.List.NonEmpty         (NonEmpty ((:|)))
 import qualified Data.List.NonEmpty         as NE
 import           Data.Proxy                 (Proxy (Proxy))
+import           Data.Typeable              (Typeable)
 import           Language.Haskell.TH.Syntax (Lift)
 import           Network.HTTP.Media         (MediaType, (//), (/:))
 
 import           WaiSample.Types.Status
 
 
-class Lift contTyp => HasContentTypes contTyp where
+data SomeContentType = forall contTyp. HasContentTypes contTyp => SomeContentType (Proxy contTyp)
+
+
+class (Typeable contTyp, Lift contTyp) => HasContentTypes contTyp where
   contentTypes :: Proxy contTyp -> NE.NonEmpty MediaType
-  matchContentType :: MediaType -> Proxy contTyp -> Maybe (Proxy someContTyp)
+
+  matchContentType :: MediaType -> Proxy contTyp -> Maybe SomeContentType
+  matchContentType mt contTypP =
+    if mt == NE.head (contentTypes contTypP)
+      then Just $ SomeContentType contTypP
+      else Nothing
 
 
 -- TODO: Remove value constructors in the types in this module.
@@ -44,10 +55,24 @@ instance HasContentTypes PlainText where
   contentTypes _ = "text" // "plain" /: ("charset", "UTF-8") :| []
 
 
+-- TODO: 次回実装する
+data ContentTypes (contTyps :: [*])
+
+
 -- | The "Choose Content-Type" type
+-- FIXME: 次回消す
 data contTyp1 |&| contTyp2 = contTyp1 :|&| contTyp2 deriving Lift
 
 instance HasStatusCode (contTyp1 |&| contTyp2)
+-- type JsonAndText = Json |&| Text
+-- Json |&| Text |&| FormUrlEncoded
+-- JsonAndText |&| FormUrlEncoded
 
 instance (HasContentTypes contTyp1, HasContentTypes contTyp2) => HasContentTypes (contTyp1 |&| contTyp2) where
   contentTypes _ = contentTypes (Proxy :: Proxy contTyp1) <> contentTypes (Proxy :: Proxy contTyp2)
+  matchContentType mt contTypP =
+    -- FIXME: 次回直す
+    let mts = contentTypes contTypP
+     in case F.find (== mt) mts of
+            Just foundMt -> matchContentType mt foundMt resObj
+            Nothing      -> matchContentType mt (NE.head mts) resObj
