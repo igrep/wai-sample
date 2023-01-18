126297cd72414c10537ee7263fc302cbb0317d5d
YAMAMOTO Yuji <igrep@n.email.ne.jp>
2023-01-18 18:29:58 +0900

    I found it impossible to make `addHeader` a variadic function

--- src/WaiSample/Types/Response/Headers.hs
+++ src/WaiSample/Types/Response/Headers.hs
@@ -8,6 +8,7 @@
 {-# LANGUAGE TypeApplications      #-}
 {-# LANGUAGE TypeFamilies          #-}
 {-# LANGUAGE TypeOperators         #-}
+{-# LANGUAGE UndecidableInstances  #-}
 
 module WaiSample.Types.Response.Headers where
 
@@ -38,11 +39,30 @@ class BuildHeaders (accumHeaders :: [Type]) (headers :: [Type]) (resObj :: Type)
   type BuildHeadersResult (accumHeaders :: [Type]) (headers :: [Type]) (resObj :: Type)
   buildHeaders :: BuildHeadersArg accumHeaders headers resObj -> BuildHeadersResult accumHeaders headers resObj
 
-instance BuildHeaders accumHeaders '[] resObj where
-  type BuildHeadersArg accumHeaders '[] resObj = resObj
-  type BuildHeadersResult accumHeaders '[] resObj = WithHeaders accumHeaders resObj
-  -- TODO: accumHeaders から先頭の要素を取り出し、引数にする
-  buildHeaders = NoHeaders
+instance BuildHeaders accumHeaders headers resObj => BuildHeaders (accumHeader ': accumHeaders) headers resObj where
+  type BuildHeadersArg (accumHeader ': accumHeaders) headers resObj = accumHeader
+  type BuildHeadersResult (accumHeader ': accumHeaders) headers resObj = BuildHeadersResult accumHeaders headers resObj
+  buildHeaders header = AddHeader header
+  --                                     ^ ここに、 buildHeader @accumHeaders headers resObj が返す WithHeaders を渡す
+  --                                     ^ そのためには、 BuildHeadersResult が WithHeaders を返すことを保証しないといけない
+  --                                     ^ だが実際には、 BuildHeadersResult は WithHeaders ではなく（最終的に WithHeaders を返す）関数かも知れないし、 WithHeaders かも知れない
+  --                                     ^ なので何らかの形で、組み立て途中の WithHeaders 型の値を渡せないといけないが、普通の関数合成ではそんなことできません！
+
+type family Accumulate (accumHeaders :: [Type]) (headers :: [Type]) :: [Type] where
+  Accumulate accumHeaders '[]                 = accumHeaders
+  Accumulate accumHeaders (header ': headers) = Accumulate (header ': accumHeaders) headers
+
+data Exp = Exp {
+    args       :: [String]
+  , resultType :: [String]
+} deriving Show
+
+bAccum :: [String] -> [String] -> Int -> Exp
+bAccum accum [] i = Exp { args = bExp accum i, resultType = accum }
+ where
+  bExp []       i = [show i]
+  bExp (x : xs) i = x : bExp xs i
+bAccum accum (x : xs) i = bAccum (x : accum) xs i
 
 class WrapWithHeaders (headers :: [Type]) (resObj :: Type) where
   type Wrapper headers resObj
