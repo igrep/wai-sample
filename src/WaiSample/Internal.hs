26872010a679e8f6f796f1842a9fa3ae3fb4278d
YAMAMOTO Yuji <igrep@n.email.ne.jp>
2023-10-11 18:23:37 +0900

    Run formatter and fix a linter warning

--- src/WaiSample/Internal.hs
+++ src/WaiSample/Internal.hs
@@ -1,13 +1,13 @@
-{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TemplateHaskellQuotes #-}
 
 module WaiSample.Internal where
 
-import qualified Data.ByteString.Char8 as B
-import           Network.HTTP.Types        (Method, methodPost)
-import qualified Network.HTTP.Types.Status as HTS
+import qualified Data.ByteString.Char8      as B
+import           Network.HTTP.Types         (Method, methodPost)
+import qualified Network.HTTP.Types.Status  as HTS
 -- For older GHC: import Language.Haskell.TH (Q, TExp, stringE)
 -- For older GHC: import Language.Haskell.TH.Syntax (unsafeTExpCoerce)
-import Language.Haskell.TH.Syntax (Code, Quote, liftTyped)
+import           Language.Haskell.TH.Syntax (Code, Quote, liftTyped)
 
 
 -- For older GHC: liftHttpStatus :: HTS.Status -> Q (TExp HTS.Status)
