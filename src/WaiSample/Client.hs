df3e3e4033c8aebd307ebbea04c8ab2ae99c9937
YAMAMOTO Yuji <igrep@n.email.ne.jp>
2024-02-07 19:07:59 +0900

    Remove oudated debug code

--- src/WaiSample/Client.hs
+++ src/WaiSample/Client.hs
@@ -83,7 +83,6 @@ declareClient prefix = fmap concat . mapM declareEndpointFunction
               let uri = URI.encode $(p)
                   rawReqHds :: RequestHeaders
                   rawReqHds = $(hd)
-              print rawReqHds
               res <- $(varE bd) $(liftByteString meth) uri rawReqHds
               let headerName = CI.mk $ B.pack "Content-Type"
                   contentTypeFromServer = lookup headerName $ responseHeaders res
