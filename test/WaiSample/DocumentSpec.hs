e4678d496cdb7f49113242279426e3de736f51f3
YAMAMOTO Yuji <igrep@n.email.ne.jp>
2024-10-09 18:26:31 +0900

    (TESTS DO NOT PASS) Add tests to `showHandlerSpec`

--- test/WaiSample/DocumentSpec.hs
+++ test/WaiSample/DocumentSpec.hs
@@ -13,11 +13,22 @@ import           WaiSample.Sample
 
 spec :: Spec
 spec = describe "showHandlerSpec" $ do
-  it "returns getExampleRequestHeaders' path, response headers, and request headers information" $ do
+  it "returns getExampleRequestHeaders' path, query parameters, response headers, and request headers information" $ do
     let getExampleRequestHeaders =
           findJust (\hdr -> handlerName hdr == "getExampleRequestHeaders") sampleRoutes
         expected = "getExampleRequestHeaders \"GET\" /exampleRequestHeaders/\n\
                    \  Request:\n\
+                   \    Query Params: (none)\n\
                    \    Headers: (X-API-VERSION: Integer | X-API-REVISION: Integer) & X-API-KEY: Text\n\
                    \  Response: (Json,ExampleRequestHeaders)\n"
+    showHandlerSpec getExampleRequestHeaders `shouldBe` expected
+
+  it "returns getExampleQueryParams path, query parameters, response headers, and request headers information" $ do
+    let getExampleRequestHeaders =
+          findJust (\hdr -> handlerName hdr == "getExampleQueryParams") sampleRoutes
+        expected = "getExampleQueryParams \"GET\" /exampleQueryParams/\n\
+                   \  Request:\n\
+                   \    Query Params: (apiVersion: Integer | apiRevision: Integer) & apiKey: Text\n\
+                   \    Headers: (none)\n\
+                   \  Response: (Json,ExampleQueryParams)\n"
     showHandlerSpec getExampleRequestHeaders `shouldBe` expected
